cf033fc25d0f69523c7f39566674c44e343473ff
AM Steen <mubbymom@centralaveconsult.com>
2020-08-14 14:49:52 -0500

    fixed shape discrepancy and got test_GetResults() to work

--- testSuite.py
+++ testSuite.py
@@ -2,19 +2,17 @@ import load_data as ld
 import pickle
 import numpy as np
 
-# TODO: Figure out why the shape of getOneHotOut is (1500,30) but
-#       shape of testCorrectOut is (1500, 3)
-# TODO: Get test_GetResults() to pass when getOneHotOut and testCorrectOut
-#       are compared.
 
 def test_GetResults():
     testInput = pickle.load(open("correctOneHotIn.pickle", "rb"))
     testCorrectOut = pickle.load(open("correctOneHotOut.pickle", "rb"))
 
-    getOneHotOut = ld.get_onehot(testInput, None)
+    getOneHotOut = ld.get_onehot(testInput, None, num_classes=3, seq_len=20)
 
 #    print(testInput)
 #    print(testCorrectOut)
 
-    assert getOneHotOut[1].shape == testCorrectOut[1].shape
+    for x,y in zip(testCorrectOut,getOneHotOut):
+        assert np.equal(x,y).all()
+    #assert getOneHotOut[1].shape == testCorrectOut[1].shape
 #    assert np.equal(getOneHotOut[1], testCorrectOut[1])
